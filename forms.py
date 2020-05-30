f6c559af3eb8020ba9ec2f79f7b905e790ab90c2
Connor Pautz <cmp998@mst.edu>
2020-05-29 22:22:41 -0500

    Added Description to form

--- forms.py
+++ forms.py
@@ -4,6 +4,7 @@ from wtforms.validators import DataRequired
 
 class CreateRecipeForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description')
     ingredients = StringField('Ingredients', validators=[DataRequired()])
     instructions = StringField('Instructions', validators=[DataRequired()])
     notes = StringField('Notes')
